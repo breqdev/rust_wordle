8bacb1185358e754ae3596595fe69e2d4c4746c0
Brooke <breq@breq.dev>
2022-03-15 22:14:47 -0400

    use type aliases

--- main.rs
+++ main.rs
@@ -1,11 +1,8 @@
 use colored::Colorize;
 
-#[derive(PartialEq, Eq, Copy, Clone)]
-struct Word {
-  letters: [char; 5],
-}
+type Word = [char; 5];
 
-#[derive(PartialEq, Eq, Copy, Clone)]
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 enum Color {
   White,
   Gray,
@@ -13,65 +10,45 @@ enum Color {
   Green,
 }
 
-#[derive(PartialEq, Eq, Copy, Clone)]
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 struct Square {
   color: Color,
   letter: char,
 }
 
-#[derive(PartialEq, Eq, Copy, Clone)]
-struct Row {
-  squares: [Square; 5],
-}
+type Row = [Square; 5];
 
 fn score_guess(target: &Word, guess: &Word) -> Row {
-  // Set up the guess as an array of Optional
-  let mut remaining = target.letters.map(|c| Some(c));
+  let mut remaining = target.map(|c| Some(c));
 
-  // Initialize the result
-  let mut row = Row {
-    squares: guess.letters.map(|letter| Square {
-      color: Color::White,
-      letter: letter,
-    }),
-  };
+  let mut result = guess.map(|letter| Square {
+    color: Color::White,
+    letter,
+  });
 
-  // Color the green squares
-  for (i, square) in row.squares.iter_mut().enumerate() {
-    if target.letters[i] == guess.letters[i] {
+  for (i, square) in result.iter_mut().enumerate() {
+    if target[i] == guess[i] {
       square.color = Color::Green;
       remaining[i] = None;
     }
   }
 
-  // Color the yellow squares
-  for (i, square) in row.squares.iter_mut().enumerate() {
-    if square.color == Color::Green {
-      continue;
-    }
-
-    let pos = remaining.iter().position(|&c| c == Some(guess.letters[i]));
-
-    if let Some(pos) = pos {
-      square.color = Color::Yellow;
-      remaining[pos] = None;
+  for (i, square) in result.iter_mut().enumerate() {
+    if square.color == Color::White {
+      if let Some(pos) = remaining.iter().position(|&c| c == Some(guess[i])) {
+        square.color = Color::Yellow;
+        remaining[pos] = None;
+      }
     }
   }
 
-  // Color remaining squares gray
-  for square in row.squares.iter_mut() {
-    if square.color == Color::Green {
-      continue;
+  for square in result.iter_mut() {
+    if square.color == Color::White {
+      square.color = Color::Gray;
     }
-
-    if square.color == Color::Yellow {
-      continue;
-    }
-
-    square.color = Color::Gray;
   }
 
-  row
+  result
 }
 
 fn print_colored(c: &Color, s: &str) {
@@ -84,48 +61,32 @@ fn print_colored(c: &Color, s: &str) {
 }
 
 fn print_row(word: &Row) {
-  // Print the top border
-  for square in word.squares.iter() {
+  for square in word.iter() {
     print_colored(&square.color, "┌───┐");
-
     print!("  ");
   }
-
   println!("");
 
-  // Print the letter
-  for square in word.squares.iter() {
+  for square in word.iter() {
     let mut boxed = "│ ".to_owned();
     boxed.push_str(&square.letter.to_string());
     boxed.push_str(" │");
 
     print_colored(&square.color, &boxed);
-
     print!("  ");
   }
-
   println!("");
 
-  // Print the bottom border
-  for square in word.squares.iter() {
+  for square in word.iter() {
     print_colored(&square.color, "└───┘");
-
     print!("  ");
   }
-
   println!("");
 }
 
 fn main() {
-  println!("Hello, wordle!");
-
-  let target = Word {
-    letters: ['A', 'R', 'R', 'A', 'Y'],
-  };
-
-  let guess = Word {
-    letters: ['F', 'U', 'R', 'R', 'Y'],
-  };
+  let target = ['A', 'R', 'R', 'A', 'Y'];
+  let guess = ['F', 'U', 'R', 'R', 'Y'];
 
   let scored = score_guess(&target, &guess);
 
