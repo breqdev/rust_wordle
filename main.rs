a5cd1d60d85aa9fb4da65ef8f10888cf1e36cf05
Brooke <breq@breq.dev>
2022-03-15 23:33:24 -0400

    use traits, allowlist json

--- main.rs
+++ main.rs
@@ -1,6 +1,32 @@
 use colored::Colorize;
+use rand::seq::SliceRandom;
+use serde_json::Value;
+use std::fs;
+use std::io;
+use std::io::Write;
 
 type Word = [char; 5];
+
+trait AsWord {
+  fn as_word(&self) -> Result<Word, ()>;
+}
+
+impl AsWord for String {
+  fn as_word(&self) -> Result<Word, ()> {
+    let mut result = [0 as char; 5];
+
+    if self.len() != 5 {
+      return Err(());
+    }
+
+    self
+      .chars()
+      .zip(result.iter_mut())
+      .for_each(|(c, square)| *square = c);
+
+    Ok(result)
+  }
+}
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 enum Color {
@@ -10,6 +36,15 @@ enum Color {
   Green,
 }
 
+fn print_colored(c: &Color, s: &str) {
+  match c {
+    Color::White => print!("{}", s.on_white()),
+    Color::Gray => print!("{}", s.on_truecolor(127, 127, 127)),
+    Color::Yellow => print!("{}", s.on_yellow()),
+    Color::Green => print!("{}", s.on_green()),
+  }
+}
+
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 struct Square {
   color: Color,
@@ -17,6 +52,36 @@ struct Square {
 }
 
 type Row = [Square; 5];
+
+trait PrintWordle {
+  fn print_wordle(&self);
+}
+
+impl PrintWordle for Row {
+  fn print_wordle(&self) {
+    for square in self.iter() {
+      print_colored(&square.color, "┌───┐");
+      print!("  ");
+    }
+    println!("");
+
+    for square in self.iter() {
+      let mut boxed = "│ ".to_owned();
+      boxed.push_str(&square.letter.to_string());
+      boxed.push_str(" │");
+
+      print_colored(&square.color, &boxed);
+      print!("  ");
+    }
+    println!("");
+
+    for square in self.iter() {
+      print_colored(&square.color, "└───┘");
+      print!("  ");
+    }
+    println!("");
+  }
+}
 
 fn score_guess(target: &Word, guess: &Word) -> Row {
   let mut remaining = target.map(|c| Some(c));
@@ -51,44 +116,87 @@ fn score_guess(target: &Word, guess: &Word) -> Row {
   result
 }
 
-fn print_colored(c: &Color, s: &str) {
-  match c {
-    Color::White => print!("{}", s.on_white()),
-    Color::Gray => print!("{}", s.on_truecolor(127, 127, 127)),
-    Color::Yellow => print!("{}", s.on_yellow()),
-    Color::Green => print!("{}", s.on_green()),
-  }
+fn load_wordlist(path: &str) -> Vec<String> {
+  let file = fs::read_to_string(path).expect("Could not open wordlist");
+
+  let data: Value = serde_json::from_str(&file).expect("Could not parse wordlist");
+
+  let words: Vec<String> = data
+    .as_array()
+    .expect("Wordlist is not an array")
+    .iter()
+    .map(|word| word.as_str().expect("Word is not a string").to_uppercase())
+    .collect();
+
+  words
 }
 
-fn print_row(word: &Row) {
-  for square in word.iter() {
-    print_colored(&square.color, "┌───┐");
-    print!("  ");
-  }
-  println!("");
+fn prompt() -> String {
+  print!("{}", "> ");
+  io::stdout().flush().unwrap();
 
-  for square in word.iter() {
-    let mut boxed = "│ ".to_owned();
-    boxed.push_str(&square.letter.to_string());
-    boxed.push_str(" │");
+  let mut input = String::new();
+  io::stdin()
+    .read_line(&mut input)
+    .expect("Could not read line");
 
-    print_colored(&square.color, &boxed);
-    print!("  ");
-  }
-  println!("");
-
-  for square in word.iter() {
-    print_colored(&square.color, "└───┘");
-    print!("  ");
-  }
-  println!("");
+  input.trim().to_uppercase()
 }
 
 fn main() {
-  let target = ['A', 'R', 'R', 'A', 'Y'];
-  let guess = ['F', 'U', 'R', 'R', 'Y'];
+  let allowed = load_wordlist("allowlist.json");
+  let targetlist = load_wordlist("targetlist.json");
 
-  let scored = score_guess(&target, &guess);
+  let mut target: Word;
+  let mut guess: Word;
 
-  print_row(&scored);
+  let mut input: String;
+
+  loop {
+    println!("Welcome to WORDLE! Enter a five letter guess...");
+
+    target = targetlist
+      .choose(&mut rand::thread_rng())
+      .unwrap()
+      .clone()
+      .as_word()
+      .expect("Wordlist yielded word of invalid length");
+
+    loop {
+      input = prompt();
+
+      if input.len() != 5 {
+        println!("Please enter a five letter guess.");
+        continue;
+      }
+
+      if input.chars().any(|c| !c.is_ascii_alphabetic()) {
+        println!("Please enter only US English letters.");
+        continue;
+      }
+
+      if !allowed.contains(&input) {
+        println!("Not in wordlist.");
+        continue;
+      }
+
+      guess = input.as_word().unwrap();
+
+      let scored = score_guess(&target, &guess);
+      scored.print_wordle();
+
+      if guess == target {
+        println!("You win!");
+        break;
+      }
+    }
+
+    println!("Play again? (y/n)");
+
+    input = prompt();
+
+    if input.to_lowercase() == "n" {
+      break;
+    }
+  }
 }
